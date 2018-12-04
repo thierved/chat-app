32fdd8ea2e632ac19f1922e9e3a8eb0043fb1131
thierno sadou diallo <thierved01@gmail.com>
2018-12-04 18:31:25 +0000

    added socket.io

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+io.on('connection', socket => {
+    console.log('new connection.')
+});
 
+io.on('disconnection', socket => {
+    console.log('disconnected.')
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
