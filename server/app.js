60cfe359800a90c0bc3fdb42eca79cba044341b2
thierno sadou diallo <thierved01@gmail.com>
2018-12-04 17:30:27 +0000

    fixed some errors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+//app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) =>{
     res.send('<h1>Welcome to the hood!</h1>');
