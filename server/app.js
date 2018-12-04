08e2aeafc415a3de3089f3fe123fc139f2a7e9bc
thierno sadou diallo <thierved01@gmail.com>
2018-12-04 17:39:21 +0000

    fixed some errors

--- server/app.js
+++ server/app.js
@@ -4,11 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) =>{
-    res.send('<h1>Welcome to the hood!</h1>');
-});
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
